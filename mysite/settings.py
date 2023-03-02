4abca48d3bad39a8ef177554657c6deef8024e42
Parigi Sai Teja Reddy <saitejareddy@10-18-245-0.dynapool.wireless.nyu.edu>
2023-03-02 18:56:47 -0500

    push

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '83ag2%k5rhmyo+pae8@pe%b5&^5a9&3ebhqdu(w96hb2e0@=g4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-kznw3pym.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
